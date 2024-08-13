31fb83577c326e1616a637f410e6182d7084f8ee
arnetkc <arnetkc@gmail.com>
2024-08-13 12:39:30 +0530

    message from

--- YTable/src/App.jsx
+++ YTable/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+const tableData = [
+  { date: "2022-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-01", views: 100, article: "Article 1" },
+
+  { date: "2023-09-02", views: 150, article: "Article 2" },
+
+  { date: "2023-09-02", views: 120, article: "Article 3" },
+
+  { date: "2020-09-03", views: 200, article: "Article 4" },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(tableData);
+
+  const handleSortDate = () => {
+    const newData = [...data];
+    const filterData = newData.sort((a, b) => {
+      if (b.views === a.views) {
+        return b.date - a.date;
+      } else {
+        return b.views - a.views;
+      }
+    });
+    console.log(filterData);
+
+    setData(filterData);
+  };
+
+  const handleSortViews = () => {
+    const newData = [...data];
+    const filterData = newData.sort((a, b) => {
+      if (b.views === a.views) {
+        return b.date - a.date;
+      } else {
+        return b.views - a.views;
+      }
+    });
+    console.log(filterData);
+
+    setData(filterData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Date and Views Table</h1>
+      <div className="button-container">
+        <button onClick={handleSortDate}>Sort by Date</button>
+        <button onClick={handleSortViews}>Sort by Views</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, i) => {
+            return (
+              <tr key={`${item.article}_${i}`}>
+                <td>{item.date}</td>
+                <td>{item.views}</td>
+                <td>{item.article}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
